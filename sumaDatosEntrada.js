73687da57b80764f22e2a83b88509db85d51fd7e
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-02-27 17:35:34 -0500

    programa que solicita 2 numeros y los suma

---
+++ sumaDatosEntrada.js
@@ -0,0 +1,9 @@
+/*
+Ejercicio 3
+
+Escribe un programa que le pida al usuario ingresar un número, luego le pide un segundo número, 
+e imprima en la consola la suma de los dos números que ingresó el usuario.
+*/
+const numero1=parseInt(prompt("ingrese valor de numero1: "))
+const numero2=parseInt(prompt("ingrese valor de numero2: "))
+console.log(`la suma es: ${numero1+numero2}`)
