e9df48654e7fb2db409a05fbfdd631daa2018a64
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-02-27 17:41:49 -0500

    programa para calcular la edad,solicitando la fecha de nacimiento

---
+++ calcularAnos.js
@@ -0,0 +1,12 @@
+/*
+Ejercicio 4
+
+Escribe un programa que le pida al usuario su año de nacimiento e imprima su edad actual en la consola 
+con la frase "Tienes X años". 
+Por ejemplo, asumiendo que el año actual es 2020 y el usuario ingresa 2000, el programa debe imprimir en la consola:
+
+Tienes 20 años
+*/
+//ejecutar en el navegador
+const fechaNacimiento=parseInt(prompt("ingrese su año de nacimiento"))
+console.log(`Tienes ${2022-fechaNacimiento} años`)
