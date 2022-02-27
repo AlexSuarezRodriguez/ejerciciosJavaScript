b9fce6c7396b27dc3665577363c10dc98cb936d1
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-02-27 17:30:00 -0500

    programa solicita nombre y lo imprime con un mensaje adicional

---
+++ saludoPrompt.js
@@ -0,0 +1,10 @@
+/*
+Ejercicio 2
+
+Escribe un programa que le pregunte al usuario su nombre e imprima "Hola " seguido del nombre y un signo
+ de exclamación.
+Por ejemplo, si el usuario ingresa "Pedro" el programa debe imprimir en la consola "Hola Pedro!".
+*/
+//ejecutarlo en el navegador
+const nombre=prompt("cual es su nombre")
+console.log(`hola ${nombre}!`)
