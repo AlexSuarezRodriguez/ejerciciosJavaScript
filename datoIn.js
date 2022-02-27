398ae68406b79add1092956290b127443cdf484a
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-02-27 17:22:30 -0500

    programa para solicitar un dato y imprimirlo

---
+++ datoIn.js
@@ -0,0 +1,10 @@
+/*
+Ejercicio 1
+
+Escribe un programa que le pida al usuario ingresar una frase y la imprima en la consola.
+
+Recuerda que para pedirle al usuario que escriba una frase debes utilizar el método prompt() 
+y para escribir en la consola debes utilizar el método console.log().
+*/
+const dato=prompt("ingrese un dato")//ejecutarlo en el navegador
+console.log(dato) 
